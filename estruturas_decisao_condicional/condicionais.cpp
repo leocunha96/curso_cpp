df8a1c8eafd6c6aaf91fec01640924599a3f3fd2
Leonardo Cunha <leonardocunha.a@gmail.com>
2023-01-14 12:59:54 -0300

    alguns if/else foram alterados para condicionais ternarios, além disso foi adicionado switch case

--- estruturas_decisao_condicional/condicionais.cpp
+++ estruturas_decisao_condicional/condicionais.cpp
@@ -10,6 +10,8 @@ int main () {
     bool bSalaryDeposited;
     bool bAccess;
     char choice;
+    int cinema;
+    int beach;
 
     string accessPassword = "potato";
     string enteredPassword;
@@ -19,31 +21,70 @@ int main () {
 
     if (enteredPassword == accessPassword) {
         cout << "\nAcesso Permitido!" << "\n";
+        
         cout << "Esta fazendo sol hoje? (Digite S para SIM ou N para NAO)" << "\n";
         cin >> choice;
-        if (choice == 'S' || choice == 's') {
-            bSunny = true;
-        } else {
-            bSunny = false;
-        }
+        (choice == 'S' || choice == 's') ? bSunny = true : bSunny = false;
+        
         cout << "O carro ficou pronto? (Digite S para SIM ou N para NAO)" << "\n";
         cin >> choice;
-        if (choice == 'S' || choice == 's') {
-            bCarReady = true;
-        } else {
-            bCarReady = false;
-        }
+        (choice == 'S' || choice == 's') ? bCarReady = true : bCarReady = false; 
+        
         cout << "O salario foi depositado? (Digite S para SIM ou N para NAO)" << "\n";
         cin >> choice;
-        if (choice == 'S' || choice == 's') {
-            bSalaryDeposited = true;
-        } else {
-            bSalaryDeposited = false;
-        }
+        (choice == 'S' || choice == 's') ? bSalaryDeposited = true : bSalaryDeposited = false;
+        
         if (bSunny && bCarReady && bSalaryDeposited) {
             cout << "\nVamos para a praia hoje!" << "\n";
+            cout << "\nDigite para qual praia vamos:\n1 - Guaratuba\n2 - Caioba\n3 - Praia de Leste\n4 - Pontal do Parana\n5 - Ilha do Mel" << "\n";
+            cin >> beach;
+
+            switch (beach) {
+            case 1: 
+                cout << "\nVamos para Guaratuba!" << "\n";
+                break;
+            case 2: 
+                cout << "\nVamos para Caioba!" << "\n";
+                break;
+            case 3: 
+                cout << "\nVamos para Praia de Leste!" << "\n";
+                break;
+            case 4: 
+                cout << "\nVamos para Pontal do Parana!" << "\n";
+                break;
+            case 5: 
+                cout << "\nVamos para Ilha do Mel!" << "\n";
+                break;
+            default:
+                cout << "\nAinda nao decidimos kkj" << "\n";
+                break;
+            }
+
         } else {
             cout << "\nVamos para o cinema hoje!" << "\n";
+            cout << "\nDigite qual filme vamos assistir:\n1 - Detona Ralph\n2 - Avatar\n3 - Guardios da Galaxia\n4 - Harry Potter e a camera secreta\n5 - Batman" << "\n";
+            cin >> cinema;
+
+            switch (cinema) {
+            case 1: 
+                cout << "\nVamos ver Detona Ralph!" << "\n";
+                break;
+            case 2: 
+                cout << "\nVamos ver Avatar!" << "\n";
+                break;
+            case 3: 
+                cout << "\nVamos ver Guardios da Galaxia!" << "\n";
+                break;
+            case 4: 
+                cout << "\nVamos ver Harry Potter e a camera secreta!" << "\n";
+                break;
+            case 5: 
+                cout << "\nVamos ver Batman" << "\n";
+                break;
+            default:
+                cout << "\nAinda nao decidimos kkj" << "\n";
+                break;
+            }
         }
 
     } else {
